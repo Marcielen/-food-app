590f36168e52d9aa6dec439126e4d20cb3413e05
Marcielen Barbosa <marcielenmmb@hotmail.com>
2023-07-03 20:29:31 -0300

    feat: erros in build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 import dns from "dns";
 
 dns.setDefaultResultOrder("verbatim");
