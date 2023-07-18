e997b788a4f276abc1e9e7f6d552bae9c94bb25d
Marcielen Barbosa <marcielenmmb@hotmail.com>
2023-07-18 09:48:42 -0300

    feat: create layout dashboard

--- frontend/src/constants/webServices.ts
+++ frontend/src/constants/webServices.ts
@@ -3,16 +3,20 @@ export const EnumWebServices = {
   SESSION: "/session",
   ME: "/me",
 
+  PRODUCT: "/product-list",
+  PRODUCT_REMOVE: "/product-remove",
+  PRODUCT_CREATE: "/product-create",
+
+  ITEMS: "/items",
+  ITEMS_CREATE: "/items-create",
+  ITEMS_REMOVE: "/items-remove",
+  ITEMS_UPDATE: "/items-update",
+
   CATEGORY: "/category",
+  CATEGORY_CREATE: "/category-create",
+  CATEGORY_REMOVE: "/category-remove",
 
-  PRODUCT: "/product",
-  PRODUCT_CATEGORY: " ",
-
-  ORDER: "/order",
-  ORDER_SEND: "/order/send",
-  ORDER_ADD: "/order/add",
-  ORDER_REMOVE: "/order/remove",
-  ORDER_LIST: "/orders",
-  ORDER_DETAIL: "/orders/detail",
-  ORDER_FINISH: "/orders/finish",
+  BUY: "/buy",
+  BUY_CREATE: "/buy-create",
+  BUY_REMOVE: "/buy-remove",
 };
--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,7 +1,31 @@
+import { EnumWebServices } from "constants/webServices";
+import { useEffect, useCallback } from "react";
+import { api } from "service/api";
+
 export const Dashboard = () => {
+  const getDataBuy = useCallback(async () => {
+    const response = await api.get(EnumWebServices.BUY);
+    console.log(response);
+  }, []);
+
+  useEffect(() => {
+    getDataBuy();
+  }, [getDataBuy]);
+
   return (
     <div>
-      <div>oi</div>
+      <div
+        style={{
+          backgroundImage: "linear-gradient(to left, #81007F, #ff7426)",
+        }}
+        className="w-full text-white px-7 py-7 h-52 rounded-lg bg-black"
+      >
+        <p className="text-lg font-bold">Total sales in the month:</p>
+        <p className="text-6xl pt-3 font-bold">$40.50</p>
+      </div>
     </div>
   );
 };
+function useCallbacks(arg0: () => void, arg1: never[]) {
+  throw new Error("Function not implemented.");
+}
